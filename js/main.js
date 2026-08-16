887c2f8a3129a9f45f2398e84cbaebe49a7398c3
Ирина Журавлева <irinaazhuravlevaaa@gmail.com>
2026-08-17 00:28:51 +0300

    Add upload editor controls

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ import {loadPictures} from './api.js';
 import {initPictureFilters} from './picture-filters.js';
 import {drawPictureCards} from './picture-cards.js';
 import {showPicturesLoadError} from './messages.js';
+import {initUploadEditor} from './upload-editor.js';
+import {initUploadEffects} from './upload-effects.js';
+import {initUploadScale} from './upload-scale.js';
+
+initUploadScale();
+initUploadEffects();
+initUploadEditor();
 
 loadPictures()
   .then((pictures) => {
---
+++ js/upload-editor.js
@@ -0,0 +1,95 @@
+import {isEscEvent} from './utils.js';
+import {resetUploadEffect} from './upload-effects.js';
+import {resetUploadScale} from './upload-scale.js';
+
+const DEFAULT_PREVIEW = 'img/upload-default-image.jpg';
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
+const uploadFormElement = document.querySelector('.img-upload__form');
+const uploadFileElement = document.querySelector('.img-upload__input');
+const uploadOverlayElement = document.querySelector('.img-upload__overlay');
+const uploadCancelElement = document.querySelector('.img-upload__cancel');
+const uploadPreviewImageElement = document.querySelector('.img-upload__preview img');
+const effectPreviewElements = document.querySelectorAll('.effects__preview');
+const hashtagsElement = document.querySelector('.text__hashtags');
+const descriptionElement = document.querySelector('.text__description');
+
+let previewUrl;
+
+const isTextFieldFocused = () => document.activeElement === hashtagsElement || document.activeElement === descriptionElement;
+
+const setPreviewImage = (src) => {
+  uploadPreviewImageElement.src = src;
+  effectPreviewElements.forEach((preview) => {
+    preview.style.backgroundImage = `url(${src})`;
+  });
+};
+
+const clearPreviewUrl = () => {
+  if (previewUrl) {
+    URL.revokeObjectURL(previewUrl);
+    previewUrl = '';
+  }
+};
+
+const resetUploadEditor = () => {
+  uploadFormElement.reset();
+  uploadFileElement.value = '';
+  resetUploadScale();
+  resetUploadEffect();
+  clearPreviewUrl();
+  setPreviewImage(DEFAULT_PREVIEW);
+};
+
+const closeUploadEditor = () => {
+  uploadOverlayElement.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  resetUploadEditor();
+};
+
+function onDocumentKeydown(evt) {
+  if (isEscEvent(evt) && !isTextFieldFocused()) {
+    evt.preventDefault();
+    closeUploadEditor();
+  }
+}
+
+const openUploadEditor = () => {
+  uploadOverlayElement.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const showSelectedPicture = () => {
+  const file = uploadFileElement.files[0];
+
+  if (!file) {
+    return;
+  }
+
+  const fileName = file.name.toLowerCase();
+  const isValidFile = FILE_TYPES.some((type) => fileName.endsWith(type));
+
+  if (!isValidFile) {
+    return;
+  }
+
+  clearPreviewUrl();
+  previewUrl = URL.createObjectURL(file);
+  setPreviewImage(previewUrl);
+};
+
+const initUploadEditor = () => {
+  uploadFileElement.addEventListener('change', () => {
+    showSelectedPicture();
+    openUploadEditor();
+  });
+
+  uploadCancelElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    closeUploadEditor();
+  });
+};
+
+export {closeUploadEditor, initUploadEditor, resetUploadEditor};
